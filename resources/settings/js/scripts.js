066816db98d13e6c789f8272755778d2a833fb5a
Johannes <j.wielsch@outlook.de>
2023-12-01 22:39:48 +0100

    script.js error-handling, removed redundant DOM

--- resources/settings/js/scripts.js
+++ resources/settings/js/scripts.js
@@ -1,43 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener('DOMContentLoaded', async () => {
+  // Handler for closing settings window
   document.getElementById('closeSettings').addEventListener('click', () => {
     window.electronAPI.closeSettingsWindow();
   });
 
+  // Load menu items
   const sidebar = document.querySelector('.sidebar ul');
-  loadMenuItems(sidebar);
+  try {
+    await loadMenuItems(sidebar);
+  } catch (error) {
+    console.error('Fehler beim Laden der Menüelemente:', error);
+  }
 
+  // Handler for fullscreen setting
   document.body.addEventListener('change', (event) => {
     if (event.target.matches('#fullscreenCheckbox')) {
       window.electronAPI.setFullscreen(event.target.checked);
     }
   });
 
-});
-
-document.addEventListener('DOMContentLoaded', async () => {
+  // Load app version
   const version = await window.electronAPI.getAppVersion();
   document.getElementById('appVersion').textContent = version;
+
+  // Initialize settings
+  initializeSettings();
 });
 
-function initializeSettings() {
-  const fullscreenCheckbox = document.getElementById('fullscreenCheckbox');
-  if (fullscreenCheckbox) {
-    window.electronAPI.getFullscreenSetting((value) => {
-      fullscreenCheckbox.checked = value;
-    });
-  }
-}
-
 async function loadMenuItems(sidebar) {
-  const menuItems = await window.menuAPI.getMenuItems();
-    
-  menuItems.forEach(item => {
-    const li = document.createElement('li');
-    li.textContent = item.charAt(0).toUpperCase() + item.slice(1);
-    li.addEventListener('click', () => loadMenuItem(item));
-    sidebar.appendChild(li);
-  });
+  try {
+    const menuItems = await window.menuAPI.getMenuItems();
+    menuItems.forEach(item => {
+      const li = document.createElement('li');
+      li.textContent = item.charAt(0).toUpperCase() + item.slice(1);
+      li.addEventListener('click', () => loadMenuItem(item));
+      sidebar.appendChild(li);
+    });
+  } catch (error) {
+    throw new Error('Fehler beim Abrufen der Menüelemente: ' + error.message);
+  }
 }
 
 async function loadMenuItem(item) {
