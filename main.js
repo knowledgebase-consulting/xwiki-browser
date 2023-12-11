ed802eb40797efcafee34e610380d0916085c7ba
de-johannes <j.wielsch@outlook.de>
2023-12-11 19:08:46 +0100

    restore previous code

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Const initialisieren
-const { app, BrowserWindow, Menu, Tray, nativeImage, ipcMain } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const { createMenu } = require('./resources/main/menu.js');
 const { autoUpdater } = require('electron-updater');
-const { TrayGenerator }  = require('./resources/main/tray.js');
+const TrayGenerator  = require('./resources/main/tray.js');
 const isDev = require('electron-is-dev');
 const process = require('process');
 const path = require('path');
@@ -122,18 +122,17 @@ function createSettingsWindow() {
 }
 // Event-Listener für die Electron-App
 // Das Hauptfenster starten und das Menü laden
-let tray = null;
 app.on('ready', () => {
-  createMainWindow();
-  const menu = createMenu(createSettingsWindow, settingsWindow);
-  Menu.setApplicationMenu(menu);
-
-  if (!isMac) {
-    const iconPath = path.join(__dirname, 'build/icon.png');
-    const icon = nativeImage.createFromPath(iconPath);
-    tray = new Tray(icon);
-    const trayGenerator = new TrayGenerator(mainWindow, createSettingsWindow, tray);
-    trayGenerator.createTray();
+  try {
+    createMainWindow();
+    const menu = createMenu(createSettingsWindow, settingsWindow);
+    const tray = new TrayGenerator(mainWindow, createSettingsWindow);
+    if (!isMac) {
+      tray.createTray();
+    }
+    Menu.setApplicationMenu(menu);
+  } catch (error) {
+    console.error('Fehler beim App-Start: ', error);
   }
 });
 // Auf dem Mac das Fenster nur schließen aber nicht das Programm beenden
--- resources/main/tray.js
+++ resources/main/tray.js
@@ -1,56 +1,60 @@
-const { Menu } = require('electron');
+const { Tray, Menu } = require("electron");
+const isDev = require('electron-is-dev');
+const path = require("path");
 
 class TrayGenerator {
-  constructor(mainWindow, createSettingsWindowFn, tray) {
+  constructor(mainWindow, createSettingsWindowFn) {
+    this.tray = null;
     this.mainWindow = mainWindow;
     this.createSettingsWindow = createSettingsWindowFn;
-    this.tray = tray;
   }
-
-  getWindowPosition() {
+  getWindowPosition = () => {
     const windowBounds = this.mainWindow.getBounds();
     const trayBounds = this.tray.getBounds();
-    const x = Math.round(trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2);
+    const x = Math.round(
+      trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
+    );
     const y = Math.round(trayBounds.y + trayBounds.height);
     return { x, y };
-  }
-
-  showWindow() {
+  };
+  showWindow = () => {
     const position = this.getWindowPosition();
     this.mainWindow.setPosition(position.x, position.y, false);
     this.mainWindow.show();
     this.mainWindow.setVisibleOnAllWorkspaces(true);
     this.mainWindow.focus();
     this.mainWindow.setVisibleOnAllWorkspaces(false);
-  }
-
-  toggleWindow() {
+  };
+  toggleWindow = () => {
     if (this.mainWindow.isVisible()) {
       this.mainWindow.hide();
     } else {
       this.showWindow();
     }
-  }
-
-  createTray() {
-    try {
-      const contextMenu = Menu.buildFromTemplate([
-        {
-          label: 'Einstellungen',
-          click: () => this.createSettingsWindow()
-        },
-        { role: 'toggleDevTools' },
-        { type: 'separator' },
-        { role: 'quit' }
-      ]);
-
-      this.tray.setToolTip('Meine Electron-Anwendung');
-      this.tray.setContextMenu(contextMenu);
-      this.tray.on('click', () => this.toggleWindow());
-    } catch (error) {
-      console.error(`Fehler beim Erstellen des Tray: ${error.message}`);
-    }
-  }
+  };
+  createTray = () => {
+    const basePath = isDev ? __dirname : app.getAppPath();
+    const iconPath = path.resolve(basePath, '../images/icon.ico');
+    this.tray = new Tray(iconPath);
+  
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'Einstellungen',
+        click: () => {
+          this.createSettingsWindow();
+        }
+      },
+      {
+        role: 'quit',
+        accelerator: 'Command+Q',
+      },
+    ]);
+  
+    this.tray.setContextMenu(contextMenu);
+    this.tray.setIgnoreDoubleClickEvents(true);
+  
+    this.tray.on("click", this.toggleWindow);
+  };
 }
 
-module.exports = { TrayGenerator };
+module.exports =  TrayGenerator;
